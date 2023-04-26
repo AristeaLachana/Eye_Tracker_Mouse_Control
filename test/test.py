19ed52a8a0d26a6d89bfe6406749f6005ec64ab6
Eleni Navrozidou <72340658+elennavr@users.noreply.github.com>
2023-04-26 12:51:42 +0200

    changed staring time

--- test/test.py
+++ test/test.py
@@ -26,15 +26,12 @@ def gaze_data_callback(gaze_data):
     # Get the gaze point coordinates in pixels
     gaze_point = gaze_data['left_gaze_point_on_display_area']
     gaze_x = int(gaze_point[0] * pyautogui.size().width) - 2
-    gaze_y = int(gaze_point[1] * pyautogui.size().height) - 4
+    gaze_y = int(gaze_point[1] * pyautogui.size().height) - 2
     # Move the mouse cursor to the gaze point coordinates
-    pyautogui.moveTo(gaze_x, gaze_y, 1)
+    pyautogui.moveTo(gaze_x, gaze_y, 0.5)
 
     # if gazing at the object's width add 1 to the staring time
-    # if object_pos[0] <= gaze_point[0] <= object_pos[0] + object_size[0]:
-    #   staring_time += 1
-
-    if abs(gaze_prev[0] - gaze_x) <= 80 or abs(gaze_prev[1] - gaze_y) <= 80:
+    if abs(gaze_prev[0] - gaze_x) <= 150 or abs(gaze_prev[1] - gaze_y) <= 150:
         staring_time += 1
         not_staring_time = 0
     else:
@@ -47,15 +44,14 @@ def gaze_data_callback(gaze_data):
         staring_time = 0
 
     # if staring_time is more than 5 seconds then click the component
-    if staring_time == 4:
+    if staring_time == 1:
         pyautogui.click()
         not_staring_time = 0
         staring_time = 0
-
-    print(staring_time)
 
 
 while True:
     eyetracker.subscribe_to(tr.EYETRACKER_GAZE_DATA, gaze_data_callback, as_dictionary=True)
     time.sleep(1)
     eyetracker.unsubscribe_from(tr.EYETRACKER_GAZE_DATA, gaze_data_callback)
+
